06f4e46da1d4daec4de86bf8fcc69cead898415c
Agustina Siri <siriagustina2706@gmail.com>
2020-12-28 19:24:49 -0300

    menu-nav

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NavBar(){
+    return ( 
+        <div>
+            <div>
+                <ul style={{display:'flex', alignContent:'center', alignItems:'stretch', listStyle:'none'}}>
+                    <li><a style={{ textDecoration:'none', color:'black', padding:'10px'}} href="#">Home</a></li>
+                    <li><a style={{ textDecoration:'none', color:'black', padding:'10px'}} href="#">About</a></li>      
+                    <li><a style={{ textDecoration:'none', color:'black', padding:'10px'}} href="#">Products</a></li>      
+                    <li><a style={{ textDecoration:'none', color:'red', padding:'10px'}} href="#">Help</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
